554d6d3572fa114623536c7b7a0c47839fbf3b27
Anthony Wong <awong88@berkeley.edu>
2024-09-08 21:26:22 -0700

    feat:added checkbox component

---
+++ src/components/checkboxes.jsx
@@ -0,0 +1,10 @@
+export default function CheckBoxes () {
+    return (
+        <form>
+            <label >Engine Oil</label>
+            <input type="checkbox" name="good" for="engine"/>
+            <input type="checkbox" name="good" for="engine"/>
+            <input type="checkbox" name="good" for="engine"/>
+        </form>
+    )
+}

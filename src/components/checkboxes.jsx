f68a7e52563e3242af4de360852cd6edef74b2ac
Anthony Wong <awong88@berkeley.edu>
2024-09-10 16:22:48 -0700

    mpi box started

--- src/components/checkboxes.jsx
+++ src/components/checkboxes.jsx
@@ -1,10 +1,16 @@
-export default function CheckBoxes () {
-    return (
-        <form>
-            <label >Engine Oil</label>
-            <input type="checkbox" name="good" for="engine"/>
-            <input type="checkbox" name="good" for="engine"/>
-            <input type="checkbox" name="good" for="engine"/>
-        </form>
-    )
+export default function CheckBoxes() {
+  return (
+    <div className="flex border border-black w-64 rounded-md">
+      <label className="p-1 mx-auto">Engine Oil</label>
+      <div className="bg-green-500 h-full w-8 p-1">
+        <input type="checkbox" name="good" for="engine" />
+      </div>
+      <div className="bg-yellow-500 h-full w-8 p-1">
+        <input type="checkbox" name="good" for="engine" />
+      </div>
+      <div className="bg-red-500 h-full w-8 p-1">
+        <input type="checkbox" name="good" for="engine" />
+      </div>
+    </div>
+  );
 }

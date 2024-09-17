6a6307c0ae0227f9b19f1c0b2b91feb959cce577
Anthony Wong <awong88@berkeley.edu>
2024-09-17 14:45:54 -0700

    fix: print style for tires

--- src/components/tires.jsx
+++ src/components/tires.jsx
@@ -20,7 +20,7 @@ export default function TiresAndBrakes({ wheel }) {
   };
 
   return (
-    <div className="border w-full md:basis-1/2">
+    <div className="border w-full md:basis-1/2 print:basis-1/2">
 
       <span className="font-semibold text-sm border w-full flex bg-gray-200 justify-center border-black">
         {wheel}

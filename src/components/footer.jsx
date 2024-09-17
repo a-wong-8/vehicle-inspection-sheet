80844dd303bba424549a25efd515dee865123250
Anthony Wong <awong88@berkeley.edu>
2024-09-17 08:39:02 -0700

    fix: print and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import awr from "../images/awr-footer.png";
 
 export default function Footer() {
   return (
-    <footer className="flex border-t p-1 justify-center">
+    <footer className="flex p-1 justify-center">
       <a href="mailto:awrautogroup@gmail.com">
         <img src={awr} alt="awr auto group logo" className="h-4" />
       </a>
